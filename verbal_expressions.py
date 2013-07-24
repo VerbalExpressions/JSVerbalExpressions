ccfded8519aecdf4028ccc78d11a87eb0cf7a807
wenjun.yan <wenjun.yan@gree.co.jp>
2013-07-24 19:31:30 +0900

    pep8

--- verbal_expressions.py
+++ verbal_expressions.py
@@ -1,16 +1,18 @@
 import re
 import inspect
 
+
 def re_escape(fn):
     def arg_escaped(this, *args):
         t = [isinstance(a, VerEx) and a.s or re.escape(a) for a in args]
         return fn(this, *t)
     return arg_escaped
 
+
 class VerEx(object):
     '''
     --- VerbalExpressions class ---
-    the following methods behave different from the original js lib. BE CAREFUL!
+    the following methods behave different from the original js lib!
 
     - end_of_line
     - start_of_line
@@ -23,8 +25,7 @@ class VerEx(object):
     '''
     def __init__(self):
         self.s = ''
-        self.modifiers = { 'I': 0,
-                           'M': 0 }
+        self.modifiers = {'I': 0, 'M': 0}
 
     def __getattr__(self, attr):
         ''' any other function will be sent to the regex object '''
@@ -35,6 +36,17 @@ class VerEx(object):
         self.s += value
         return self
 
+    def regex(self):
+        ''' get a regular expression object. '''
+        return re.compile(self.s, self.modifiers['I'] | self.modifiers['M'])
+        compile = regex
+
+    def source(self):
+        ''' return the raw string'''
+        return self.s
+        raw = value = source
+
+    # ---------------------------------------------
 
     def anything(self):
         return self.add('(.*)')
@@ -71,7 +83,7 @@ class VerEx(object):
 
     @re_escape
     def range(self, *args):
-        from_tos =  [args[i:i+n] for i in range(0, len(args), n)]
+        from_tos = [args[i:i+n] for i in range(0, len(args), n)]
         return self.add(''.join(['-'.join(i) for i in from_tos]))
 
     def tab(self):
@@ -80,7 +92,18 @@ class VerEx(object):
     def word(self):
         return self.add("(\\w+)")
 
-    # ---------------------------------------
+    def OR(self, value=None):
+        ''' `or` is a python keyword so we use `OR` instead. '''
+        self.add("|")
+        return self.find(value) if value else self
+
+    def replace(self, string, repl):
+        return self.sub(repl, string)
+
+    # --------------- modifiers ------------------------
+
+    # no global option. It depends on which method
+    # you called on the regex object.
 
     def with_any_case(self, value=False):
         self.modifiers['I'] = re.I if value else 0
@@ -89,24 +112,3 @@ class VerEx(object):
     def search_one_line(self, value=False):
         self.modifiers['M'] = re.M if value else 0
         return self
-
-    # no global option. It depends on which method
-    # you called on the regex object.
-
-    def OR(self, value=None):
-        ''' `or` is a python keyword so we use `OR` instead. '''
-        self.add("|")
-        return self.find(value) if value else self
-
-    def regex(self):
-        ''' get a regular expression object. '''
-        return re.compile(self.s, self.modifiers['I'] | self.modifiers['M'])
-    compile = regex
-
-    def source(self):
-        ''' return the raw string'''
-        return self.s
-    raw = value = source
-
-    def replace(self, string, repl):
-        return self.sub(repl, string)
