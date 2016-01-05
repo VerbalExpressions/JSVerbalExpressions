6ff9f1890c90d46aa76e1a7a608d4a3b7faee135
softwarespot <softwarespotapps@gmail.com>
2016-01-05 21:07:19 +0200

    Added ESLint to testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,66 @@
-module.exports = function(grunt) {
+module.exports = function gruntConfig(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        eslint: {
+            options: {
+                configFile: '.eslintrc.json',
+            },
+            src: ['VerbalExpressions.js'],
+        },
+
         qunit: {
-            files: ['test/index.html']
+            files: ['test/index.html'],
         },
 
         copy: {
             build: {
                 src: '<%= pkg.name %>.js',
-                dest: 'dist/<%= pkg.name %>.js'
-            }
+                dest: 'dist/<%= pkg.name %>.js',
+            },
         },
 
         uglify: {
             options: {
                 banner: '/*!\n' +
-                        '* <%= pkg.name %> JavaScript Library v<%= pkg.version %>\n' +
-                        '* <%= pkg.homepage %>\n' +
-                        '*\n' +
-                        '*\n' +
-                        '* Released under the <%= pkg.license.type %> license\n' +
-                        '* <%= pkg.license.url %>\n' +
-                        '*\n' +
-                        '* Date: <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                        '*\n' +
-                        '*/\n',
-                sourceMap: true
+                    '* <%= pkg.name %> JavaScript Library v<%= pkg.version %>\n' +
+                    '* <%= pkg.homepage %>\n' +
+                    '*\n' +
+                    '*\n' +
+                    '* Released under the <%= pkg.license.type %> license\n' +
+                    '* <%= pkg.license.url %>\n' +
+                    '*\n' +
+                    '* Date: <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                    '*\n' +
+                    '*/\n',
+                sourceMap: true,
             },
             dist: {
-                files: {'dist/<%= pkg.name %>.min.js' : ['<%= pkg.name %>.js']}
-            }
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= pkg.name %>.js'],
+                },
+            },
         },
 
         sourcemap_localize: {
             options: {
-                localize_to : '..'
+                localize_to: '..',
             },
             build: {
                 files: {
-                    src: ['dist/*.min.map']
-                }
-            }
-        }
+                    src: ['dist/*.min.map'],
+                },
+            },
+        },
     });
 
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-qunit");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('gruntify-eslint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-sourcemap-localize');
-    grunt.registerTask('test', 'qunit');
+
+    grunt.registerTask('test', ['eslint', 'qunit']);
     grunt.registerTask('default', ['qunit']);
     grunt.registerTask('build', ['qunit', 'copy', 'uglify', 'sourcemap_localize']);
 };
