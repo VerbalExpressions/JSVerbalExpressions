02eddda280d700fff4fb4a113f32efc8be74036a
softwarespot <softwarespotapps@gmail.com>
2016-01-10 11:22:25 +0200

    Updated grunt packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,9 +55,9 @@ module.exports = function gruntConfig(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-sourcemap-localize');
 
     grunt.registerTask('test', ['eslint', 'qunit']);
