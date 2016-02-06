a84dfcefb5760bc28d892501a7fa101070c0cdd3
SignpostMarv <me@signpostmarv.name>
2016-02-06 21:50:48 +0000

    done tinkering with tests for now

--- test/tests.js
+++ test/tests.js
@@ -92,26 +92,38 @@ test('somethingBut', function somethingButTest() {
     ok(!testRegex.test(testString), 'Starts with an a');
 });
 
-test('startOfLine', function startOfLineTest() {
+test('startOfLine', function startOfLineTest(assert) {
     var testRegex = VerEx().startOfLine().then('a');
     var testString = 'a';
+    var withStartOfLine = '/^(?:a)/gm';
+    var withoutStartOfLine = '/(?:a)/gm';
 
     ok(testRegex.test(testString), 'Starts with an a');
 
     testRegex.lastIndex = 0;
     testString = 'ba';
     ok(!testRegex.test(testString), 'Doesn\'t start with an a');
+
+    assert.equal((testRegex.toRegExp() + ''), withStartOfLine, 'Start of line matching should be on');
+    assert.equal((testRegex.startOfLine(false).toRegExp() + ''), withoutStartOfLine, 'Start of line matching should be off');
+    assert.equal((testRegex.startOfLine().toRegExp() + ''), withStartOfLine, 'Start of line matching should be on again');
 });
 
-test('endOfLine', function endOfLineTest() {
+test('endOfLine', function endOfLineTest(assert) {
     var testRegex = VerEx().find('a').endOfLine();
     var testString = 'a';
+    var withEndOfLine = '/(?:a)$/gm';
+    var withoutEndOfLine = '/(?:a)/gm';
 
     ok(testRegex.test(testString), 'Ends with an a');
 
     testRegex.lastIndex = 0;
     testString = 'ab';
     ok(!testRegex.test(testString), 'Doesn\'t end with an a');
+
+    assert.equal((testRegex.toRegExp() + ''), withEndOfLine, 'End of line matching should be on');
+    assert.equal((testRegex.endOfLine(false).toRegExp() + ''), withoutEndOfLine, 'End of line matching should be off');
+    assert.equal((testRegex.endOfLine().toRegExp() + ''), withEndOfLine, 'End of line matching should be on again');
 });
 
 test('maybe', function maybeTest() {
@@ -136,15 +148,41 @@ test('anyOf', function anyOfTest() {
     ok(!testRegex.test(testString), 'Doesn\'t have an x, y, or z after an a');
 });
 
-test('or', function orTest() {
+test('or', function orTest(assert) {
     var testRegex = VerEx().startOfLine().then('abc').or('def');
     var testString = 'defzzz';
+    var abcdef = '/^(?:(?:abc))|(?:(?:def))/gm';
+    var abcdefghi = '/^(?:(?:(?:abc))|(?:(?:def))|(?:(?:ghi)))/gm';
+    var abcdefghi_ = '/^(?:(?:(?:(?:abc))|(?:(?:def))|(?:(?:ghi))|(?:)))/gm';
 
     ok(testRegex.test(testString), 'Starts with an abc or a def');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('abc'), 'abc should match');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('def'), 'def should match');
+    testRegex.lastIndex = 0;
+    assert.notOk(testRegex.test('ghi'), 'ghi should not match match');
+    assert.equal((testRegex.toRegExp() + ''), abcdef, 'Checking regex');
 
     testRegex.lastIndex = 0;
     testString = 'xyzabc';
     ok(!testRegex.test(testString), 'Doesn\'t start with an abc or a def');
+
+    testRegex.or('ghi');
+    assert.equal((testRegex.toRegExp() + ''), abcdefghi, 'Checking regex');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('abc'), 'abc should match');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('def'), 'def should match');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('ghi'), 'ghi should match');
+    testRegex.lastIndex = 0;
+    assert.notOk(testRegex.test('jkl'), 'jkl should not match');
+
+    testRegex.or();
+    assert.equal((testRegex.toRegExp() + ''), abcdefghi_, 'Checking regex');
+    testRegex.lastIndex = 0;
+    assert.ok(testRegex.test('ghi'), 'ghi should match');
 });
 
 test('lineBreak', function lineBreakTest() {
@@ -188,19 +226,22 @@ test('tab', function tabTest() {
     ok(!testRegex.test(testString), 'No tab then abc');
 });
 
-test('withAnyCase', function withAnyCaseTest() {
+test('withAnyCase', function withAnyCaseTest(assert) {
     var testRegex = VerEx().startOfLine().then('a');
     var testString = 'A';
 
-    ok(!testRegex.test(testString), 'Not case-insensitive');
+    assert.notOk(testRegex.test(testString), 'Not case-insensitive');
 
-    testRegex = VerEx().startOfLine().then('a').withAnyCase();
-    testString = 'A';
-    ok(testRegex.test(testString), 'Case-insensitive');
+    testRegex.withAnyCase();
+    testRegex.addModifier('i'); // here for lazy coverageness
+    assert.ok(testRegex.test(testString), 'Not case-insensitive');
+    testRegex.withAnyCase(false);
+    assert.notOk(testRegex.test(testString), 'Not case-insensitive');
 
+    testRegex.withAnyCase();
     testRegex.lastIndex = 0;
     testString = 'a';
-    ok(testRegex.test(testString), 'Case-insensitive');
+    assert.ok(testRegex.test(testString), 'Not case-insensitive');
 });
 
 test('whitespace', function whitespaceTest() {
@@ -214,7 +255,7 @@ test('whitespace', function whitespaceTest() {
     ok(!testRegex.test(testString), 'a, then no whitespace and then z');
 });
 
-test('searchOneLine', function searchOneLineTest() {
+test('searchOneLine', function searchOneLineTest(assert) {
     var testRegex = VerEx().startOfLine().then('a').br().then('b').endOfLine();
     var testString = 'a\nb';
 
@@ -223,6 +264,13 @@ test('searchOneLine', function searchOneLineTest() {
     testRegex = VerEx().startOfLine().then('a').br().then('b').endOfLine().searchOneLine();
     testString = 'a\nb';
     ok(testRegex.test(testString), 'b is on the second line, but we are only searching the first');
+
+
+    testRegex = VerEx().startOfLine().then('b').searchOneLine();
+    assert.notOk(testRegex.test(testString), 'b is on the second line, but we are only searching the first');
+
+    testRegex.searchOneLine(false);
+    assert.ok(testRegex.test(testString), 'b should now be found on the second line');
 });
 
 test('sanitize', function sanitizeTest() {
@@ -270,11 +318,41 @@ test('multiple', function multipleTest() {
     ok(!testRegex.test(testString), 'Should be \'foo\' repeated two to five times');
 });
 
-test('getRegex', function getRegexTest() {
+test('getRegex', function getRegexTest(assert) {
     var regex = (
-        VerEx().startOfLine().range(0, 9, 'a', 'z', 'A', 'Z').multiple('')
+        VerEx().startOfLine().range(0, 9, 'a', 'z', 'A', 'Z').multiple('').word().any('egrx').multiple(5)
     );
+    var expecting = '/^[0-9a-zA-Z](?:)*\\w+[egrx](?:5)*/gm';
 
-    ok((regex.toRegExp() + '') === '/^[0-9a-zA-Z](?:)*/gm');
-    ok((regex + '') === '/^[0-9a-zA-Z](?:)*/gm');
+    assert.equal((regex.toRegExp() + ''), expecting);
+    assert.equal((regex + ''), expecting);
+});
+
+
+test('replace', function replaceTest(assert) {
+    var regex = VerEx().find('(');
+    assert.equal(regex.replace(':(', ')'), ':)', 'Should have replaced changed ":(" to ":)"');
+    assert.equal(regex.replace(':(:(', ')'), ':):)', 'Should be to smiles');
+    regex.stopAtFirst(true);
+    assert.equal(regex.replace(':(:(', ')'), ':):(', 'Should be one smile');
+    regex.stopAtFirst(false);
+    assert.equal(regex.replace(':(:(', ')'), ':):)', 'Should be to smiles');
+});
+
+test('repeatPrevious', function repeatPreviousTest(assert) {
+    var testRegex = VerEx().find('a');
+
+    assert.equal((testRegex.toRegExp() + ''), '/(?:a)/gm');
+
+    testRegex.repeatPrevious(0, 1);
+    assert.equal((testRegex.toRegExp() + ''), '/(?:a){0,1}/gm');
+
+    testRegex.repeatPrevious('foo', 'bar');
+    assert.equal((testRegex.toRegExp() + ''), '/(?:a){0,1}/gm');
+});
+
+test('capture', function beginCaptureTest(assert) {
+    var testRegex = VerEx().beginCapture().anyOf('abcdefg').endCapture();
+
+    assert.equal((testRegex.toRegExp() + ''), '/([abcdefg])/gm');
 });
