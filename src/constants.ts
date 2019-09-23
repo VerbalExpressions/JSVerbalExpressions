643c5eba26b21a73d2a8d3edd95604c58a30acf1
Shreyas Minocha <sm-git@riseup.net>
2019-09-23 17:38:45 +0530

    Add more constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,15 @@
 import RawExpression from "./types/raw-expression";
 
+const raw = String.raw;
+
 export const startOfLine = new RawExpression("^");
 export const endOfLine = new RawExpression("$");
+
+export const digit = new RawExpression(raw`\d`);
+export const wordCharacter = new RawExpression(raw`\w`);
+export const whitespaceCharacter = new RawExpression(raw`\s`);
+export const wordBoundary = new RawExpression(raw`\b`);
+export const anyCharacter = new RawExpression(".");
+
 export const anything = new RawExpression(".*");
 export const something = new RawExpression(".+");
