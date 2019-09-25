fa46fe7841e1221a0d84528835b996f192ac4d52
Shreyas Minocha <sm-git@riseup.net>
2019-09-25 12:05:53 +0530

    Create some test stubs

---
+++ test/sanitize.test.ts
@@ -0,0 +1,7 @@
+import sanitize from "../src/util/sanitize";
+
+describe("sanitize(input)", () => {
+  it.todo("should escape escape-worthy characters");
+  it.todo("should not unnecessarily escape characters");
+  it.todo("should not incorrectly escape characters");
+});
