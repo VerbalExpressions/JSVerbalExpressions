f3f070de5ab816960fecc30bd6a975de6dae6433
Shreyas Minocha <sm-git@riseup.net>
2019-11-10 01:26:23 +0530

    Set up bundling with rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import multiEntry from 'rollup-plugin-multi-entry';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/*.ts',
+  output: [
+    { file: 'dist/index.js', format: 'esm' },
+    { file: 'dist/umd.js', name: 'VerEx', format: 'umd' }
+  ],
+  plugins: [
+    multiEntry(),
+    typescript()
+  ]
+};
