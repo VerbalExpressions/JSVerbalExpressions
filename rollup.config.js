b79079b9ad258e33a13e6a759eaffab4341acd98
Shreyas Minocha <sm-git@riseup.net>
2019-11-14 08:37:25 +0530

    Configure minified builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import multiEntry from "rollup-plugin-multi-entry";
 import typescript from "rollup-plugin-typescript2";
+import {terser} from "rollup-plugin-terser";
 
 export default {
   input: "src/*.ts",
   output: [
-    {file: "dist/index.js", format: "esm"},
-    {file: "dist/umd.js", name: "VerEx", format: "umd"}
+    {file: "dist/index.mjs", format: "esm"},
+    {file: "dist/index.min.mjs", format: "esm"},
+    {file: "dist/umd.js", name: "ve", format: "umd"},
+    {file: "dist/umd.min.js", name: "ve", format: "umd"}
   ],
   plugins: [
     multiEntry(),
-    typescript()
+    typescript(),
+    terser({
+      include: [/^.+\.min\.m?js$/],
+      output: {comments: /[^\w\W]/}
+    })
   ]
 };
