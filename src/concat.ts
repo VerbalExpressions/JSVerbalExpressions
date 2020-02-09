0bc4f7798cf983fa2a59d3c81d5db954c4243721
Shreyas Minocha <sm-git@riseup.net>
2020-02-09 22:54:22 +0530

    Simplify concat code

--- src/concat.ts
+++ src/concat.ts
@@ -1,12 +1,3 @@
 import {group} from "./group";
-import Expression from "./types/expression";
-import Fragment, {fragmentsFromExpressions, joinFragments} from "./types/fragment";
 
-function concat(...expressions: Expression[]): Fragment {
-  const fragments = fragmentsFromExpressions(expressions);
-  const concatenated = joinFragments(fragments);
-
-  return group.nonCapturing(concatenated);
-}
-
-export {concat};
+export const concat = group.nonCapturing;
