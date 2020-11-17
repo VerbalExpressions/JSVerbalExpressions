135176cc9ab75cb78a91aa09dc8a8b79ae149032
Jesse Luoto <jesse.luoto@gmail.com>
2020-11-17 21:45:47 +0200

    Update Babel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,22 @@
 module.exports = function gruntConfig(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
 
         eslint: {
             options: {
-                configFile: '.eslintrc',
+                configFile: ".eslintrc",
             },
-            target: ['VerbalExpressions.js', 'test/tests.js', 'Gruntfile.js'],
+            target: ["VerbalExpressions.js", "test/tests.js", "Gruntfile.js"],
         },
 
         ava: {
-            test: ['test/tests.js'],
+            test: ["test/tests.js"],
             options: {
                 nyc: true,
             },
 
             verbose: {
-                test: ['test/tests.js'],
+                test: ["test/tests.js"],
                 options: {
                     verbose: true,
                     nyc: true,
@@ -27,16 +27,14 @@ module.exports = function gruntConfig(grunt) {
         babel: {
             options: {
                 sourceMap: true,
-                presets: [
-                    ['env', { modules: false }],
-                ],
+                presets: [["@babel/preset-env", { modules: false }]],
                 plugins: [
-                    ['transform-builtin-extend', { globals: ['RegExp'] }],
+                    ["transform-builtin-extend", { globals: ["RegExp"] }],
                 ],
             },
             dist: {
                 files: {
-                    'dist/verbalexpressions.js': 'VerbalExpressions.js',
+                    "dist/verbalexpressions.js": "VerbalExpressions.js",
                 },
             },
         },
@@ -44,61 +42,74 @@ module.exports = function gruntConfig(grunt) {
         umd: {
             all: {
                 options: {
-                    src: 'dist/verbalexpressions.js',
-                    objectToExport: 'VerEx',
-                    amdModuleId: 'VerEx',
-                    globalAlias: 'VerEx',
+                    src: "dist/verbalexpressions.js",
+                    objectToExport: "VerEx",
+                    amdModuleId: "VerEx",
+                    globalAlias: "VerEx",
                 },
             },
         },
 
         uglify: {
             options: {
-                banner: '/*!\n'
-                    + '* <%= pkg.name %> JavaScript Library v<%= pkg.version %>\n'
-                    + '* <%= pkg.homepage %>\n'
-                    + '*\n'
-                    + '* Released under the <%= pkg.license %> license\n'
-                    + '*/\n',
+                banner:
+                    "/*!\n" +
+                    "* <%= pkg.name %> JavaScript Library v<%= pkg.version %>\n" +
+                    "* <%= pkg.homepage %>\n" +
+                    "*\n" +
+                    "* Released under the <%= pkg.license %> license\n" +
+                    "*/\n",
                 sourceMap: true,
             },
             dist: {
                 files: {
-                    'dist/verbalexpressions.min.js': ['dist/verbalexpressions.js'],
+                    "dist/verbalexpressions.min.js": [
+                        "dist/verbalexpressions.js",
+                    ],
                 },
             },
         },
 
         sourcemap_localize: {
             options: {
-                localize_to: '..',
+                localize_to: "..",
             },
             build: {
                 files: {
-                    src: ['dist/*.min.js.map'],
+                    src: ["dist/*.min.js.map"],
                 },
             },
         },
 
         markdownlint: {
             options: {
-                config: grunt.file.readJSON('.mdlintrc.json'),
+                config: grunt.file.readJSON(".mdlintrc.json"),
             },
-            src: ['README.md', 'docs/*.md', 'docs/VerbalExpression/*.md'],
+            src: ["README.md", "docs/*.md", "docs/VerbalExpression/*.md"],
         },
     });
 
-    grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-ava');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-umd');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-sourcemap-localize');
-    grunt.loadNpmTasks('grunt-markdownlint');
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-ava");
+    grunt.loadNpmTasks("grunt-babel");
+    grunt.loadNpmTasks("grunt-umd");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-sourcemap-localize");
+    grunt.loadNpmTasks("grunt-markdownlint");
 
-    grunt.registerTask('default', ['test']);
-    grunt.registerTask('test', ['compile', 'eslint', 'markdownlint', 'ava:test']);
-    grunt.registerTask('test:verbose', ['compile', 'eslint', 'ava:verbose']);
-    grunt.registerTask('compile', ['babel', 'umd:all']);
-    grunt.registerTask('build', ['compile', 'ava:test', 'uglify', 'sourcemap_localize']);
+    grunt.registerTask("default", ["test"]);
+    grunt.registerTask("test", [
+        "compile",
+        "eslint",
+        "markdownlint",
+        "ava:test",
+    ]);
+    grunt.registerTask("test:verbose", ["compile", "eslint", "ava:verbose"]);
+    grunt.registerTask("compile", ["babel", "umd:all"]);
+    grunt.registerTask("build", [
+        "compile",
+        "ava:test",
+        "uglify",
+        "sourcemap_localize",
+    ]);
 };
